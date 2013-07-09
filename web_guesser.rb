84b65130792e56e2a851b2d0d3ad3563e5fc9332
Rosieva Ashong-Katai <rosenaadedei@aol.com>
2013-07-09 14:38:55 -0400

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = x = (1 + rand(99).to_s
+secret_number = x = (1 + rand(99)).to_s
 
 get '/' do
-  "The Secret number is: #{x}"
+  "The SECRET NUMBER is X: #{x}"
 end
