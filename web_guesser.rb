f6b51ec9a69717dec115596cd0ddada1a1fe336b
Rosieva Ashong-Katai <rosenaadedei@aol.com>
2013-07-09 16:50:15 -0400

    End of the day Tuesday!

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = x = (1 + rand(99)).to_s
+number = (1 + rand(99)).to_s
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  	if params["guess"].to_i > number.to_i + 5
+		guess = "Way Too High!"
+  	elsif params["guess"].to_i < number.to_i - 5 
+		guess = "Way Too Low!"
+  	elsif params["guess"].to_i > number.to_i
+		guess = "Too High!"
+	elsif params["guess"].to_i < number.to_i
+		guess = "Too Low!"
+	else
+		guess = "You got it right!"
+  	end
+  	erb :index, :locals => {:number => number, :guess => guess}
 end
